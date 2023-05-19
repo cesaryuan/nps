fbf4c32bf2ff5b7cec53807531b36953f64aafa0
Cesaryuan <Cesaryuan@qq.com>
2023-05-19 11:10:25 +0800

    build: ci

--- build.assets.windows.sh
+++ build.assets.windows.sh
@@ -1,10 +1,7 @@
 export GOPROXY=direct
 
-# sudo apt-get update
-# sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
-env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.dll cmd/npc/sdk.go
-env GOOS=linux GOARCH=386 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.so cmd/npc/sdk.go
-tar -czvf npc_sdk.tar.gz npc_sdk.dll npc_sdk.so npc_sdk.h
+sudo apt-get update
+sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
 
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
 
